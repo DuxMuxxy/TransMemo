4decef588fea0f8c9574dea701d81a9043b829c9
Dux Muxxy <duxmuxxy@proton.me>
2025-02-10 21:50:43 +0100

    Round decimals to 2 digits after comma

--- app/src/main/java/com/chrysalide/transmemo/presentation/extension/StringExtensions.kt
+++ app/src/main/java/com/chrysalide/transmemo/presentation/extension/StringExtensions.kt
@@ -1,9 +1,10 @@
 package com.chrysalide.transmemo.presentation.extension
 
 import androidx.core.text.isDigitsOnly
+import java.math.RoundingMode
 
 fun String.isValidIntegerValue() = isNotBlank() && isDigitsOnly()
 
 fun String.isValidDecimalValue() = isNotBlank() && matches("^[0-9]+(\\.[0-9]+)?$".toRegex())
 
-fun Float.stripTrailingZeros(): String = toBigDecimal().stripTrailingZeros().toPlainString()
+fun Float.stripTrailingZeros(): String = toBigDecimal().setScale(2, RoundingMode.HALF_UP).stripTrailingZeros().toPlainString()
